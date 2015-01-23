db12b0864f7790bd51e2ab336d4eec23ff90a9c3
Tyler Payne <tyler43636@gmail.com>
2015-01-23 17:57:44 -0500

    add method to fetch the latest message

--- src/Context/Services/MailTrap.php
+++ src/Context/Services/MailTrap.php
@@ -66,6 +66,16 @@ trait MailTrap
     }
 
     /**
+     * Fetch the latest message from MailTrap inbox.
+     * 
+     * @return mixed
+     */
+    protected function fetchLatestMessage($inboxId = null)
+    {
+        return $this->fetchInbox($inboxId)[0];
+    }
+
+    /**
      *
      * Empty the MailTrap inbox.
      *
